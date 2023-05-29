88be6c51b2a33bea84d58c862a1b47fc5be4d2cc
eamasya19 <eamasya19@ku.edu.tr>
2023-05-29 13:38:25 +0300

    now join works too

--- room-booking/reserve_join.php
+++ room-booking/reserve_join.php
@@ -24,7 +24,7 @@
         echo $res_time;
         
         // Build the SQL query
-        $sql = "INSERT INTO reservations (ku_id, building, floor, class_no, res_time, res_status, study_type)  values ('$ku_id', '$building' , '$floor' , '$class_no', '$res_time', 'JOINED', '$studyType')";
+        $sql = "INSERT INTO reservations (ku_id, building, floor, class_no, res_time, res_status, study_type)  values ('$ku_id', '$building' , '$floor' , '$class_no', '$res_time', 'RESERVED', '$studyType')";
 
         echo $sql;
         $result = mysqli_query($db, $sql);
