2022628d258d92f005e87194d0cda1d23b94c73a
bora <baktas19@ku.edu.tr>
2023-05-29 05:07:16 +0300

    just adding space :)

--- room-booking/config.php
+++ room-booking/config.php
@@ -1,10 +1,11 @@
 <?php
-mysqli_report(MYSQLI_REPORT_OFF);
-$db = mysqli_connect('localhost','root','','class_booking', '3302');
 
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
+    mysqli_report(MYSQLI_REPORT_OFF);
+    $db = mysqli_connect('localhost','root','','class_booking', '3302');
+
+    if($db->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
 
 ?>
 
